014cc750b7dd61e101c5f2be816e1b6384fb3d82
Ini Ogunsanya <156601535+IniOgunsanya@users.noreply.github.com>
2026-04-15 23:15:00 -0400

    anotner

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -8,11 +8,11 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str) -> str:
+def hash_password(password: str):
     password = password[:72]
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str) -> bool:
+def verify_password(plain_password, hashed_password):
     plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
