334102d9c8c38a14ed3a94e37b5b472c884fa8ee
Ini Ogunsanya <156601535+IniOgunsanya@users.noreply.github.com>
2026-04-15 23:20:26 -0400

    33

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -10,10 +10,12 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash_password(password: str):
     password = password[:72]
+    print("USING UPDATED AUTH FILE1")
     return pwd_context.hash(password)
 
 def verify_password(plain_password, hashed_password):
     plain_password = plain_password[:72]
+    print("USING UPDATED AUTH FILE2")
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
